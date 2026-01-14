01d08e166a2fe8d14fb8bdae2ccf2927561bd407
Muhammad Hassan <developerdeyy@gmail.com>
2026-01-14 14:45:09 +0000

    initial create
    wasd

---
+++ practicegithub/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace practicegithub.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
